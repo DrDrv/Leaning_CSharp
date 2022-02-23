28e3a27269e39e1c4ad08dcb64b2e2204036d6d1
Drv <dzen.driver@gmail.com>
2022-02-23 23:05:55 +0300

    Add Exsample 1&2

--- Examp1/Program.cs
+++ Examp1/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Who are you? - ");
+﻿Console.WriteLine("Hi, World!");
+Console.Write("Who are you? - ");
 string username = Console.ReadLine();
 Console.Write("Goodday, ");
 Console.WriteLine(username);
